935bc5623ba950ff56415635dea249b97cd353e4
guydaniels <guy@neongecko.com>
2018-02-27 17:24:46 -0800

    fix ai response for 0000.us

--- mycroft/skills/intent_service.py
+++ mycroft/skills/intent_service.py
@@ -285,6 +285,8 @@ class IntentService(object):
                     # No conversation, use intent system to handle utterance
                     intent = self._adapt_intent_match(utterances, lang)
                     # intent['flac_filename'] = ''
+                    LOG.debug('message ===== ' + str(message))
+                    LOG.debug('message.data.get("flac_filename","") ===== ' + message.data.get('flac_filename',''))
                     if message.data.get('flac_filename',''):
                         intent['flac_filename'] = message.data.get('flac_filename','')
 
